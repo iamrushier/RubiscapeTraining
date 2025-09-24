5efcc443d4604a1b81c51aec95fa8d17b69ed7bd
iamrushier <rushikeshsurve193@gmail.com>
2025-09-24 15:31:09 +0530

    Remove __pycache__ from repository

--- event_scheduler/event_scheduler/urls.py
+++ event_scheduler/event_scheduler/urls.py
@@ -5,8 +5,8 @@ from events import views as event_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', event_views.register, name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name='events/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='events/logout.html'), name='logout'),
-    path('', include('events.urls')),
+    # path('register/', event_views.register, name='register'),
+    # path('login/',auth_views.LoginView.as_view(template_name='events/login.html'), name='login'),
+    # path('logout/',auth_views.LogoutView.as_view(template_name='events/logout.html'), name='logout'),
+    # path('', include('events.urls')),
 ]
