70b609541e07529877fd33c16c2786db2c0d74c3
iamrushier <rushikeshsurve193@gmail.com>
2025-09-24 12:33:42 +0530

    Model migration and app install in settings.py

--- event_scheduler/events/models.py
+++ event_scheduler/events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Event(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField()
@@ -7,3 +7,8 @@ class Event(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    
+    def __str__(self):
+        return self.name
