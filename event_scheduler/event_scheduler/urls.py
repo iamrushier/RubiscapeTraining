9ed3270c4e89e5f0b0e25a33459d540e6df6cd9c
iamrushier <rushikeshsurve193@gmail.com>
2025-09-25 09:32:36 +0530

    Login functionality - using django default auth

--- event_scheduler/event_scheduler/urls.py
+++ event_scheduler/event_scheduler/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path,include
-from django.contrib.auth import views as auth_views
-from events import views as event_views
+from django.urls import path, include
+from django.views.generic.base import TemplateView 
+# from django.contrib.auth import views as auth_views
+# from events import views as event_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"), 
     # path('register/', event_views.register, name='register'),
     # path('login/',auth_views.LoginView.as_view(template_name='events/login.html'), name='login'),
     # path('logout/',auth_views.LogoutView.as_view(template_name='events/logout.html'), name='logout'),
