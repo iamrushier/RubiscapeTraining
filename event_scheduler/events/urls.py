e82690e44ea2d4cafefc7959a7d4eb1df81703f4
iamrushier <rushikeshsurve193@gmail.com>
2025-09-25 12:05:05 +0530

    Create new event

--- event_scheduler/events/urls.py
+++ event_scheduler/events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import my_events
+from .views import my_events,create_event
 
 urlpatterns = [
-    path('',my_events,name='my_events')
+    path('',my_events,name='my_events'),
+    path('create/',create_event,name='create_event')
 ]
--- event_scheduler/events/views.py
+++ event_scheduler/events/views.py
@@ -7,3 +7,17 @@ from .forms import EventForm
 def my_events(request):
     events = Event.objects.filter(user=request.user).order_by('date','time')
     return render(request, 'events/my_events.html', {'events':events})
+
+
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event= form.save(commit=False)
+            event.user=request.user
+            event.save()
+            return redirect('my_events')
+    else:
+        form = EventForm()
+    return render(request, 'events/create_event.html', {'form':form})
