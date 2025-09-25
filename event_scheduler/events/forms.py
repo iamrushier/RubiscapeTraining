f3689eb3bd8ed1dbccfa1a1a770ef4f25c02d17f
iamrushier <rushikeshsurve193@gmail.com>
2025-09-25 12:15:20 +0530

    Added data and time input types in create event form

--- event_scheduler/events/forms.py
+++ event_scheduler/events/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Event
+import datetime
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields=['name','date','time','description']
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'type':'date',
+                'min':datetime.date.today().strftime('%Y-%m-%d')
+            }),
+            'time':forms.TimeInput(attrs={
+                'type':'time'
+            }),
+        }
